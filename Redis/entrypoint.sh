e3348157906923f2920b1d23d9e7838459e541a4
happy-bird <whohow20094702@163.com>
2020-08-05 10:50:59 +0800

    Create entrypoint.sh

---
+++ Redis/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+if [[ "${1}" == "start" ]]; then
+  echo "Starting Redis with defaults from /etc/redis.conf"
+  exec gosu redis:redis redis-server /etc/redis.conf
+elif [ "${1:0:1}" = "-" ]; then
+  echo "Starting Redis with cli-set options only, no default config."
+  exec gosu redis:redis redis-server $@
+fi
+
+exec "$@"
